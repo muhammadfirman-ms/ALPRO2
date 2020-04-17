3278ab3f298e9d596c439220daa801cdc760d736
muhammadfirman-ms <62007079+muhammadfirman-ms@users.noreply.github.com>
2020-04-17 20:51:11 +0800

    Add files via upload

---
+++ Tugas 4 Tipe Bentukan no 2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+#include<string.h>
+
+using namespace std;
+
+
+
+int main(){
+
+	
+
+	struct buah{
+
+		string warna;
+
+		float berat;
+
+		int harga;
+
+		string rasa;
+
+	};
+
+	
+
+return 0;
+
+}
+
+
